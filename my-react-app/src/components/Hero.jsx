bd06a06f4f2e23cc85908e9e290f925e4667fdd9
jamesbrown173 <jamesbrown173@gmail.com>
2024-04-29 15:06:58 +0700

    commit

--- my-react-app/src/components/Hero.jsx
+++ my-react-app/src/components/Hero.jsx
@@ -1,139 +1,82 @@
 import {
+  easeIn,
   motion,
+  spring, // Import motion component for Framer Motion animations
   useMotionValueEvent,
   useScroll,
+  useSpring,
   useTransform,
-} from "framer-motion";
-import React, { useRef } from "react";
+} from "framer-motion"; // Import animation utilities from Framer Motion library
+import React, { useRef } from "react"; // Import React library components
 
+// Define animation parameters for each programming language
+const languageAnimations = {
+  vsCode: { x: "-20000%", y: "-50000%", rotateStart: -40, rotateEnd: -7000 },
+  python: { x: "20000%", y: "-50000%", rotateStart: 35, rotateEnd: 7000 },
+  nodeJS: { x: "-10000%", y: "-100000%", rotateStart: 20, rotateEnd: -10000 },
+  react: { x: "-30000%", y: "-100000%", rotateStart: -45, rotateEnd: -10000 },
+  // expressJS: {
+  //   x: "-30000%",
+  //   y: "-100000%",
+  //   rotateStart: 60,
+  //   rotateEnd: -10000,
+  // },
+  // framer: { x: "-20000%", y: "-100000%", rotateStart: 10, rotateEnd: -10000 },
+  // mySQL: { x: "-20000%", y: "-100000%", rotateStart: 40, rotateEnd: -10000 },
+  // tailwind: { x: "-20000%", y: "-100000%", rotateStart: 90, rotateEnd: -10000 },
+};
 function Hero({ forwardedRef }) {
-  const ref = useRef(null);
+  const ref = useRef(null); // Create a reference to a DOM element
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["start start", "end start"],
+    offset: ["start start", "end end"], // Set scroll offset for animation
   });
 
-  // Set the forwarded ref to the root div
+  // This is an example of a code comment
+  // Expose the ref to the parent component using React.useImperativeHandle
   React.useImperativeHandle(forwardedRef, () => ref.current);
 
-  // Defining where the items should move to and how they rotate
+  // Function to generate motion styles based on language and scroll position
+  const getLanguageMotionStyles = (language) => {
+    const { x, y, rotateStart, rotateEnd } = languageAnimations[language];
 
-  // VS CODE
-  const vsCodeCardY = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["-0%", "-100000%"]
-  );
-  const vsCodeCardX = useTransform(scrollYProgress, [0, 1], ["0%", "10000%"]);
-  const vsCodeCardRotate = useTransform(scrollYProgress, [0, 1], [40, 10000]); // Rotate from 0 degrees to 360 degrees
-
-  // PYTHON
-  const pythonCardY = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["-0%", "-100000%"]
-  );
-  const pythonCardX = useTransform(scrollYProgress, [0, 1], ["0", "-20000%"]);
-  const pythonCardRotate = useTransform(scrollYProgress, [0, 1], [-40, -10000]); // Rotate from 0 degrees to 360 degrees
-
-  // PYTHON
-  const nodeJSCardY = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["-0%", "-100000%"]
-  );
-  const nodeJSCardX = useTransform(scrollYProgress, [0, 1], ["0", "-10000%"]);
-  const nodeJSCardRotate = useTransform(scrollYProgress, [0, 1], [0, -10000]); // Rotate from 0 degrees to 360 degrees
+    return {
+      // Map scrollYProgress to interpolate animation values
+      y: useTransform(scrollYProgress, [0, 1], ["-0%", y]),
+      x: useTransform(scrollYProgress, [0, 1], ["0", x]),
+      rotate: useTransform(scrollYProgress, [0, 1], [rotateStart, rotateEnd]),
+    };
+  };
 
   return (
     <div
       id="hero"
-      className="h-[70dvh] flex items-center  flex-col justify-evenly"
+      className="h-[70dvh] flex items-center flex-col justify-evenly"
     >
       <h1 className="font-mono text-2xl leading-10 px-5">
         Hi! I'm James! I'm a web developer working with ...
       </h1>
 
       <div className="languagesAndSkills-container flex flex-row flex-wrap gap-2 justify-center">
-        <motion.div
-          style={{ y: vsCodeCardY, x: vsCodeCardX, rotate: vsCodeCardRotate }}
-          id="card-vscode"
-          className="languagesAndSkills-card absolute"
-        >
-          <h1 id="card-text-vscode" className="languagesAndSkills-card-text">
-            vs code
-          </h1>
-        </motion.div>
-        <motion.div
-          style={{ y: pythonCardY, x: pythonCardX, rotate: pythonCardRotate }}
-          id="card-python"
-          className="languagesAndSkills-card absolute"
-        >
-          <h1 id="card-text-python" className="languagesAndSkills-card-text">
-            python
-          </h1>
-        </motion.div>
-
-        <motion.div
-          style={{ y: nodeJSCardY, x: nodeJSCardX, rotate: nodeJSCardRotate }}
-          id="card-nodejs"
-          className="languagesAndSkills-card"
-        >
-          <h1 id="card-text-nodejs" className="languagesAndSkills-card-text">
-            nodejs
-          </h1>
-        </motion.div>
-        {/* 
-        <motion.div
-          style={{ y: item1Y, x: item1X, rotate: item1Rotate }}
-          id="card-react"
-          className="languagesAndSkills-card"
-        >
-          <h1 id="card-text-react" className="languagesAndSkills-card-text">
-            react
-          </h1>
-        </motion.div>
-        <motion.div
-          style={{ y: item1Y, x: item1X, rotate: item1Rotate }}
-          id="card-expressjs"
-          className="languagesAndSkills-card"
-        >
-          <h1 id="card-text-expressjs" className="languagesAndSkills-card-text">
-            expressjs
-          </h1>
-        </motion.div>
-        <motion.div
-          style={{ y: item1Y, x: item1X, rotate: item1Rotate }}
-          id="card-framer"
-          className="languagesAndSkills-card"
-        >
-          <h1 id="card-text-framer" className="languagesAndSkills-card-text">
-            framer
-          </h1>
-        </motion.div>
-        <motion.div
-          style={{ y: item1Y, x: item1X, rotate: item1Rotate }}
-          id="card-mysql"
-          className="languagesAndSkills-card"
-        >
-          <h1 id="card-text-mysql" className="languagesAndSkills-card-text">
-            mySQL
-          </h1>
-        </motion.div>
-        <motion.div
-          style={{ y: item1Y, x: item1X, rotate: item1Rotate }}
-          id="card-tailwind"
-          className="languagesAndSkills-card"
-        >
-          <h1 id="card-text-tailwind" className="languagesAndSkills-card-text">
-            tailwind
-          </h1>
-        </motion.div> */}
+        {/* Map through languageAnimations to render motion divs for each language */}
+        {Object.keys(languageAnimations).map((language) => (
+          <motion.div
+            key={`card-${language}`}
+            style={getLanguageMotionStyles(language)} // Apply motion styles based on language
+            className="languagesAndSkills-card absolute"
+            id={`card-${language}`}
+          >
+            <h1
+              id={`card-text-${language}`}
+              className="languagesAndSkills-card-text"
+            >
+              {language} {/* Display the language name */}
+            </h1>
+          </motion.div>
+        ))}
       </div>
     </div>
   );
 }
 
 export default Hero;
-
-// The language and skills buttons are styling in the main.css. I couldn't write the styling in tailwind.
